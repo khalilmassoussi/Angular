8be62b7663bd270417ae44788645c6b970416569
Khalil Massoussi <massoussikhalil@gmail.com>
2020-02-12 15:33:36 +0100

    mm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PatientsComponent} from './patients/patients.component';
+import {PatientDetailComponent} from './patient-detail/patient-detail.component';
+import {PatientAddComponent} from './patient-add/patient-add.component';
+import {PatientEditComponent} from './patient-edit/patient-edit.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PatientsComponent,
+        PatientDetailComponent,
+        PatientAddComponent,
+        PatientEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
